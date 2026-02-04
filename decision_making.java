66510823044f3444d3e60ea26b8dafa2496d387e
Likitha <likithasuguna2006@gmail.com>
2026-02-04 18:50:02 +0530

    D2
    Implement comparison method for two integers.

---
+++ decision_making.java
@@ -0,0 +1,21 @@
+package decision_making;
+
+public class Solution {
+
+    public static String compareNM(int n, int m) {
+        if (n < m) {
+            return "lesser";
+        } else if (n == m) {
+            return "equal";
+        } else {
+            return "greater";
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 3;
+
+        System.out.println(compareNM(n, m));
+    }
+}
