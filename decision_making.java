164cc8f7696791277316a4112a9dd05a41643bff
Likitha <likithasuguna2006@gmail.com>
2026-03-01 21:18:03 +0530

    D2

--- decision_making.java
+++ decision_making.java
@@ -1,18 +1,17 @@
 package decision_making;
-public class Solution {
-
-    public static String compareNM(int n, int m) {
-        if (n < m) {
+public class Solution{
+    public static String compareNM(int n,int m){
+        if(n<m){
             return "lesser";
-        } else if (n == m) {
+        }else if(n==m){
             return "equal";
-        } else {
+        }else {
             return "greater";
         }
     }
-    public static void main(String[] args) {
-        int n = 5;
-        int m = 3;
-        System.out.println(compareNM(n, m));
+    public static void main(String[] args){
+        int n=5;
+        int m=3;
+        System.out.println(compareNM(n,m));
     }
 }
