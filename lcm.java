2ca8cbda14488522833f06c01f40d0a4c12254fa
Likitha <likithasuguna2006@gmail.com>
2026-04-02 18:37:35 +0530

    D16

---
+++ lcm.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int LCM(int a, int b){
+        return (a * b) / gcd(a, b);
+    }
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
